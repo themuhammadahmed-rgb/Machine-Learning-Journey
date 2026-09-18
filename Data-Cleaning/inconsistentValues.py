c1e7e9aeb013f44f9d7b33b6f33c382a6b9ca894
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-18 21:45:24 +0500

    done with inconsistent values

--- Data-Cleaning/inconsistentValues.py
+++ Data-Cleaning/inconsistentValues.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
-
 data = {
-"Gender" : ["Male", "male", "MALE"]
+"Gender" : ["Male", "male", "  MALE"]
 }
+
+df = pd.DataFrame(data)
+
+df["Gender"] = df["Gender"].str.strip().str.lower()
+
+print(df)
+print(df["Gender"].value_counts())
