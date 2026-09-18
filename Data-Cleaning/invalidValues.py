21b99fb4765bc202f14958d6816110de70a416e4
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-18 22:47:40 +0500

    coverting invalid values to null

--- Data-Cleaning/invalidValues.py
+++ Data-Cleaning/invalidValues.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
-data ={
-    "Age" : [20, 25, -3, 30]
+data = {
+    "Age": [20, 25, -3, 30]
 }
 
 df = pd.DataFrame(data)
 
+print(df)
+
 print(df["Age"] > 0)
+
+df.loc[df["Age"] <= 0, "Age"] = None      
