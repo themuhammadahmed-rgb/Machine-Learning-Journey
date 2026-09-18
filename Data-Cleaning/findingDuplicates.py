a5f8d1d539a5d4d0220045ae4e324676828bebf5
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-18 20:57:41 +0500

    duplicates

---
+++ Data-Cleaning/findingDuplicates.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "Name": ["Ali", "Ahmed", "Ali", "Sara"],
+    "Age": [22, 25, 22, 30],
+    "Salary": [50000, 60000, 50000, 80000]
+}
+
+df = pd.DataFrame(data)
+
+print(df.duplicated())
+
+print(df.duplicated().sum())
+
