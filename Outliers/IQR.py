1309530315598704db166022f2a0f1e1b7bb9696
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-19 19:07:19 +0500

    lower & higher bound

--- Outliers/IQR.py
+++ Outliers/IQR.py
@@ -10,3 +10,10 @@ Q1 = df["Salary"].quantile(0.25)
 Q3 = df["Salary"].quantile(0.75)
 
 IQR = Q3 - Q1
+
+print("Q1:", Q1)
+print("Q3:", Q3)
+print(IQR)
+
+lowerBound = Q1 - (1.5 * IQR)
+upperBound = Q3 + (1.5 * IQR)
