b3b826dab24251eb40c39333e14a59c6b14cf304
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-20 15:20:26 +0500

    ordinal encoding

--- feature-engineering(again)/ordinal encoding.py
+++ feature-engineering(again)/ordinal encoding.py
@@ -5,3 +5,14 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+
+education_map = {
+    "High School": 1,
+    "Bachelor": 2,
+    "Master": 3,
+    "PhD": 4
+}
+
+df["Education"] = df["Education"].map(education_map)
+print(df)
