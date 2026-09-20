1ff95cf3aecdff7aa1f42c658c0cbc23524bc160
Muhammad Ahmed <bhaikiahalhai@gmail.com>
2026-09-20 20:40:07 +0500

    done with standard scaler

--- feature-engineering(again)/Standard Scaler.py
+++ feature-engineering(again)/Standard Scaler.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 data = {
     "Age": [20, 25, 30, 35, 40]
@@ -6,6 +7,8 @@ data = {
 
 df = pd.DataFrame(data)
 
-print(df)
-
 scaler = StandardScaler()
+
+df["Age"] = scaler.fit_transform(df[["Age"]])
+
+print(df)
